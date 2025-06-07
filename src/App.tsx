a887d1e3f76dfe65ac50053b836cbdf8ae34c638
Reza Ghavipour <rghavipor@gmail.com>
2025-06-07 08:10:05 +0330

    Added tsconfig.node.json

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-center py-8">CV Template</h1>
+    </div>
+  )
+}
+
+export default App
